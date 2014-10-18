a875dc7814954332d699d62b19e570def2160522
Gabriel Paladino <l31rb4g@gmail.com>
2014-10-17 22:40:16 -0300

    builders

--- builders/win32.sh
+++ builders/win32.sh
@@ -29,8 +29,7 @@ $_python $_pyinstaller barbacoa.py \
     --hidden-import=uuid \
     --hidden-import=gzip
 	
-mv $_build_path/barbacoa.spec $_build_path/barbacoa-orig.spec
-exp='s/exe = EXE(pyz,/exe = EXE(pyz, '$trees'/' $_build_path/barbacoa-orig.spec
+exp='s/exe = EXE(pyz,/exe = EXE(pyz, '$trees'/'
 sed -i "$exp" $_build_path/barbacoa.spec
 
 $_python $_pyinstaller barbacoa.py $_build_path/barbacoa.spec
