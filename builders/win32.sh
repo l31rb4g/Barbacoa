3dbf3d18a0366e041d8a0d6bb82b399291197d8d
Gabriel Paladino <l31rb4g@gmail.com>
2014-10-17 21:52:31 -0300

    builders

--- builders/win32.sh
+++ builders/win32.sh
@@ -5,16 +5,13 @@ _pyinstaller='C:/PyInstaller-2.1/pyinstaller.py'
 _build_path='BUILD'
 _include_paths=(bbqlib plugins www)
 
+rm -rf $_build_path
+mkdir $_build_path
+
 trees=''
 for p in ${_include_paths[@]}; do
     trees=$trees"Tree('../"$p"', prefix='"$p"'), "
 done
-
-rm -rf $_build_path
-mkdir $_build_path
-
-mv $_build_path/barbacoa.spec $_build_path/barbacoa-orig.spec
-sed 's/exe = EXE(pyz,/exe = EXE(pyz, '$trees'/' $_build_path/barbacoa-orig.spec > $_build_path/barbacoa.spec
 
 $_python $_pyinstaller barbacoa.py \
     --onefile \
@@ -23,3 +20,13 @@ $_python $_pyinstaller barbacoa.py \
     --workpath="$_build_path/build" \
     --hidden-import=uuid \
     --hidden-import=gzip
+	
+mv $_build_path/barbacoa.spec $_build_path/barbacoa-orig.spec
+sed 's/exe = EXE(pyz,/exe = EXE(pyz, '$trees'/' $_build_path/barbacoa-orig.spec > $_build_path/barbacoa.spec
+
+$_python $_pyinstaller barbacoa.py _build_path/barbacoa.spec
+
+
+echo '+--------------------------+'
+echo '| Build finished!          |'
+echo '+--------------------------+'
