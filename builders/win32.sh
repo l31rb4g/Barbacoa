7a3f88043451cdc63d55e7fed48a1b777a6b042c
Gabriel Paladino <l31rb4g@gmail.com>
2014-10-17 21:09:51 -0300

    builders

--- builders/win32.sh
+++ builders/win32.sh
@@ -4,4 +4,4 @@ _python='C:/Python27/python.exe'
 _pyinstaller='C:/PyInstaller-2.1/pyinstaller.py'
 
 
-$_python $_pyinstaller ../barbacoa.py -F --hidden-import=uuid --hidden-import=gzip
+$_python $_pyinstaller ./barbacoa.py -F --hidden-import=uuid --hidden-import=gzip
